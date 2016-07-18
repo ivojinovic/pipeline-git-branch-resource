9538e257b7349bcd4ddcf5461687f056a0631288
Ivan Vojinovic <ivan.vojinovic@hs2solutions.com>
2016-07-18 09:51:34 -0400

    Refactoring

--- assets/log_in_to_concourse.sh
+++ assets/log_in_to_concourse.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 CONCOURSE_TARGET=$1
+PAYLOAD=$2
 
-CONCOURSE_USERNAME=$(jq -r '.source.concourse_username // ""' < $payload)
-CONCOURSE_PASSWORD=$(jq -r '.source.concourse_password // ""' < $payload)
-CONCOURSE_URL=$(jq -r '.source.concourse_url // ""' < $payload)
+CONCOURSE_USERNAME=$(jq -r '.source.concourse_username // ""' < $PAYLOAD)
+CONCOURSE_PASSWORD=$(jq -r '.source.concourse_password // ""' < $PAYLOAD)
+CONCOURSE_URL=$(jq -r '.source.concourse_url // ""' < $PAYLOAD)
 
 echo -e "$CONCOURSE_USERNAME\n$CONCOURSE_PASSWORD\n" | fly -t $CONCOURSE_TARGET login --concourse-url $CONCOURSE_URL
