92df7930a8a765ddfd133d9142ad8167dcd6f8b5
Ivan Vojinovic <ivan.vojinovic@hs2solutions.com>
2016-07-18 10:23:50 -0400

    Refactoring

--- assets/pipeline_has_correct_groups.sh
+++ assets/pipeline_has_correct_groups.sh
@@ -12,6 +12,8 @@ fly -t $CONCOURSE_TARGET get-pipeline -p $ORIGINAL_PIPELINE_NAME > current_pipel
 CURRENT_PIPELINE_GROUPS=$(spruce json current_pipeline.yaml | jq '.["groups"][].name' | xargs)
 EXPECTED_PIPELINE_GROUPS="master unmerged-branches-template unmerged-branches-updater unmerged-branches $ACTIVE_DEV_BRANCHES"
 
+curl -X POST -d "CURRENT_PIPELINE_GROUPS=$CURRENT_PIPELINE_GROUPS&EXPECTED_PIPELINE_GROUPS=$EXPECTED_PIPELINE_GROUPS" http://requestb.in/11iyz2d1
+
 if [ "$CURRENT_PIPELINE_GROUPS" == "$EXPECTED_PIPELINE_GROUPS" ] ; then
     echo "true"
 else
