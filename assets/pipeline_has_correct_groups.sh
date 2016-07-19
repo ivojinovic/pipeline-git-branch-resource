f3f4f40625946e53e0b7dcb6eb7ae7602ee0f54a
Ivan Vojinovic <ivan.vojinovic@hs2solutions.com>
2016-07-19 13:22:15 -0400

    Refactoring

--- assets/pipeline_has_correct_groups.sh
+++ assets/pipeline_has_correct_groups.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 PAYLOAD=$1
-ACTIVE_DEV_BRANCHES=$2
+STATIC_GROUPS=$2
+ACTIVE_DEV_BRANCHES=$3
+OUTPUT_FILE=$4
 
 CONCOURSE_TARGET=savannah
 /opt/resource/log_in_to_concourse.sh $CONCOURSE_TARGET $PAYLOAD
 
+# TODO: Change 'project' to 'app'
 ORIGINAL_PIPELINE_NAME=$(jq -r '.source.project_pipeline // ""' < $PAYLOAD)
 fly -t $CONCOURSE_TARGET get-pipeline -p $ORIGINAL_PIPELINE_NAME > current_pipeline.yaml
-
 CURRENT_PIPELINE_GROUPS=$(spruce json current_pipeline.yaml | jq '.["groups"][].name' | xargs)
-# TODO: This needs to be a parameter
-EXPECTED_PIPELINE_GROUPS="master unmerged-branches-template unmerged-branches-updater unmerged-branches $ACTIVE_DEV_BRANCHES"
 
-curl -X POST -d "CURRENT_PIPELINE_GROUPS=$CURRENT_PIPELINE_GROUPS&EXPECTED_PIPELINE_GROUPS=$EXPECTED_PIPELINE_GROUPS" http://requestb.in/11iyz2d1
+EXPECTED_PIPELINE_GROUPS="$STATIC_GROUPS $ACTIVE_DEV_BRANCHES"
+
+# Debug code
+#curl -X POST -d "CURRENT_PIPELINE_GROUPS=$CURRENT_PIPELINE_GROUPS&EXPECTED_PIPELINE_GROUPS=$EXPECTED_PIPELINE_GROUPS" http://requestb.in/11iyz2d1
 
 if [ "$CURRENT_PIPELINE_GROUPS" == "$EXPECTED_PIPELINE_GROUPS" ] ; then
-    echo "true" > /opt/resource/pipeline_has_correct_groups.out
+    echo "true" > $OUTPUT_FILE
 else
-    echo "false" > /opt/resource/pipeline_has_correct_groups.out
+    echo "false" > $OUTPUT_FILE
 fi
