5b922aca84d8838a9af8b1942a05b1a9efbff7ee
Ivan Vojinovic <ivan.vojinovic@hs2solutions.com>
2016-07-25 20:27:13 -0400

    Removed checking for the latest pipelines project sha

--- assets/export_parameter_values.sh
+++ assets/export_parameter_values.sh
@@ -48,7 +48,6 @@ export_parameter_values() {
     # Pipelines project /
     #########
     export PARAM_PIPELINES_BW_VERSION=$(jq -r '.source.branch_watcher_version // ""' < $PAYLOAD)
-    export PARAM_PIPELINES_GIT_URI=$(jq -r '.source.pipelines_git_uri // ""' < $PAYLOAD)
     export PARAM_PIPELINES_BRANCH=$(jq -r '.source.pipelines_branch // ""' < $PAYLOAD)
 
     export CONST_PIPELINES_GIT_DIR=$TMPDIR/git-resource-repo-cache-2
